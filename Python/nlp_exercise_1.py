3424bc4f6102262cc4f266cc027bd86f12004fc4
03lizchavez <emc6332@psu.edu>
2024-02-21 02:40:33 -0500

    moving file

--- Python/nlp_exercise_1.py
+++ Python/nlp_exercise_1.py
@@ -1,3 +1,8 @@
+import spacy
+# Need line 8 the first time: Then comment it out after the first time you run it:
+# nlp = spacy.cli.download("en_core_web_md")
+nlp = spacy.load('en_core_web_md')
+
 textfile = open('nlp_exercise_1', 'r', encoding='utf8')
 words = textfile.read()
 wordstrings = str(words)
